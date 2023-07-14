a2f492914b5223fccd272d04423121e86a8f3fba
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-15 04:02:09 +0530

    Array method

---
+++ part1/52.js
@@ -0,0 +1,12 @@
+// some method 
+
+const numbers =[3,5,11,9];
+
+const ans = numbers.some((Number)=>Number%2===0);
+console.log(ans);
+
+const userCart = [
+    {productId : 1, productName: "Mobile" , price: 12000},
+    {productId : 2, productName: "Laptop" , price: 22000}, 
+    {productId : 3, productName: "TV" , price: 32000}, 
+]
