49d26f30b2d24867f43550ba1c582ad84c8d643c
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-16 03:17:28 +0530

    Map data structure

---
+++ part1/55.js
@@ -0,0 +1,26 @@
+// Maps 
+
+
+
+
+
+
+
+
+
+
+
+
+//object literal
+//key-> string
+//key->symbol
+
+
+const person = {
+    firstName : 'vinayak',
+    age: 20,
+    1:"one" 
+}
+
+console.log(person.firstName);
+console.log(person["firstName"]);
