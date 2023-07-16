13b87f66a7cdc227e7e53a0aa3471a5d7a7aa355
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-17 02:49:14 +0530

    Maps

--- part1/55.js
+++ part1/55.js
@@ -27,3 +27,11 @@
 
 const person = new Map();
 person.set('firstName', 'Vinayak');
+person.set('age', 20);
+person.set(1, 'one');
+person.set([1,2,3], 'onetwothree');
+person.set({1: 'one'}, 'onetwothree');
+console.log(person);
+for(let key of person){
+    console.log(key , typeof key);
+}
