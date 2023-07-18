568868c3a1e45e8d2d0973787745c54ed71995eb
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-19 03:44:46 +0530

    this keyword

--- part1/59.js
+++ part1/59.js
@@ -1,26 +1,26 @@
-// this keyword 
+//methods 
+//function inside object is called a method
 
-// if (true){
-//     console.log(this);
-// }
 
-//this is always  going to be window when we are outside the 
-// scope 
+ function personInfo(){
+        console.log(`person name is ${this.firstName} & ${this.age} is 20`);
+    }
 
-// function talk(){
-//     return this
-// }
-// const me = {
-//     name : 'Vina',
-//     talk
-// }
-// console.log(me);
-
-const person = {
+const person1 = {
     firstName: "vinayak",
     age : 20 ,
-    about : function(){
-        console.log("person name is vinayak & age is 20");
-    }
-}
-console.log(person.about);
+    about :personInfo
+} 
+const person2 = {
+    firstName: "amit",
+    age : 19 ,
+    about :personInfo
+} 
+const person3 = {
+    firstName: "harsh",
+    age : 18 ,
+    about :personInfo
+} 
+person2.about();
+person1.about();
+person3.about();
