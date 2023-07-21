ea5df2884d58b831788aeb5ea813c1463e71baa7
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-21 22:06:07 +0530

    call() apply() bind()

--- part1/60.js
+++ part1/60.js
@@ -1,19 +1,42 @@
-// call apply bind 
-const Name = {
-    firstname: "Vinayak",
-    lastname : "Hegde",
-}
+// // call apply bind 
+// const Name = {
+//     firstname: "Vinayak",
+//     lastname : "Hegde",
+// }
 
-Name.printFullName();
+// Name.printFullName();
 
-let printFullName =  function(){
-        console.log(this.firstname + " "+ this.lastname);
+// let printFullName =  function(){
+//         console.log(this.firstname + " "+ this.lastname);
+//     }
+
+// let name2 = {
+//     firstname: "Sachin",
+//     lastname: "Tendulkar",
+// }
+
+// //function borrowing 
+// Name.printFullName();
+
+// function hello(){
+//     console.log("hello world");
+// }
+
+// hello.call();
+
+//example 2 
+ const user1 = {
+    firstname: "meena",
+    age: 50,
+    about: function(hobby, favMusic){
+        console.log(this.firstname, this.age, hobby, favMusic);
+    }
+ }
+    const user2 = {
+        firstname: "nagveni",
+        age: 60,
     }
 
-let name2 = {
-    firstname: "Sachin",
-    lastname: "Tendulkar",
-}
 
-//function borrowing 
-Name.printFullName();
+ 
+user1.about.call(user1 , "kalimba", "Till i collapse ");
