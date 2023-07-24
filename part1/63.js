e55f943347cdc5269cf7b235161d9cb8e1381a28
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-24 17:48:13 +0530

    prototype and use of prototype

---
+++ part1/63.js
@@ -0,0 +1,8 @@
+const user1 = {
+    firstName: "vinayak",
+    age: 20,
+    about (){
+        console.log(this.firstName, this.age);
+    }
+}
+user1.about();
---
+++ part1/64.js
@@ -0,0 +1,11 @@
+// prototype 
+
+// function hello(){
+//     console.log("hello world");
+// }
+
+// javascript function ===> function + Object 
+
+//you can add your own properties
+//  hello.myOwnproperty = "very unique value";
+// console.log(hello.myOwnproperty);
