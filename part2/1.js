cedf1fc4c9ae80c9094e5bf4c357f395eca635e6
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-07-26 13:46:00 +0530

    css

---
+++ part2/1.js
@@ -0,0 +1,15 @@
+//compilation
+// code execution
+
+// why compilation
+
+// How javascript code executes 
+
+// what is global execution context ?
+// what is local execution context  ?
+// closures 
+
+console.log(this);
+console.log(window);
+console.log(firstname);
+var firstname = 'vinayak';
